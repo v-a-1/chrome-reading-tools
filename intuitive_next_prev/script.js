b1d680165c543598159a4957ead4f501e9fce809
v-a-1 <varun.m.ajani@gmail.com>
2012-10-15 04:16:28 +0530

    Added unrelated functionality. Change html width so that number of words per line can be controlled. This repo to be converted to reading tools. Also going to add "Invert" functionality, where dark background and light text can be inverted to read better

--- intuitive_next_prev/script.js
+++ intuitive_next_prev/script.js
@@ -28,3 +28,61 @@ $(document).live('keydown', function (e) {
 		window.location = prev_href;
 	}
 });
+
+// Sometimes the text occupies the full width of the browser which is too long to read on big screens.
+// So ctrl + shift + - will reduce the width of the text without affecting it's font size.
+// ctrl + shift + + increases width (even beyond the bounds of the browser width).
+// ctrl + shift + 0 resets to original size.
+// This might not work correctly if any css properties have been attached to <html>
+
+// Before anything else, check if this domain had its width previously
+// modified. If so, set that as the width;
+
+
+
+var keys = {};
+
+$(document).keydown(function (e) {
+    keys[e.which] = true;
+});
+
+$(document).keyup(function (e) {
+    delete keys[e.which];
+});
+var existing_width = $('html').width();
+// Set any pre-existing width.
+chrome.storage.sync.get('html_width', function(items){
+	var html_width = items.html_width !== undefined ? items.html_width : false;
+    existing_width = (html_width !== false && html_width[document.domain] !== undefined) ? html_width[document.domain] : false;
+	$('html').css({'width': existing_width, 'margin': '0 auto'});
+});
+
+var html_width = existing_width;
+// shift: 16, control: 17, +: 187, -:189, 0:48
+$(document).keydown(function (e) {
+	if(16 in keys && 17 in keys){ // Shift Control
+		if(187 in keys){ // Increase html width '+'
+			$('html').css({'width': html_width + 80, 'margin': '0 auto'});
+			html_width = $('html').width();
+			save_html_width(html_width);
+		}else if(189 in keys){ // Decrease html width '-'
+			$('html').css({'width': html_width - 80, 'margin': '0 auto'});
+			html_width = $('html').width();
+			save_html_width(html_width);
+		}else if(48 in keys){ // Reset html width '0'
+			$('html').css({'width': 'auto', 'margin': 'auto'});
+			html_width = $('html').width();
+			save_html_width(html_width);
+		}
+	}
+});
+
+
+function save_html_width(width){
+	var html_width = {};
+	chrome.storage.sync.get('html_width', function(items){
+		html_width = items !== undefined ? items : {};
+	});
+	html_width[document.domain] = width;
+	chrome.storage.sync.set({'html_width': html_width});
+}
