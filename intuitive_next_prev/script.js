6718c7df387ca340bedbb992d9cd1e313fe006a0
v-a-1 <varun.m.ajani@gmail.com>
2012-10-14 22:31:22 +0530

    Done. Most likely won't add any other features or make any further changes.

--- intuitive_next_prev/script.js
+++ intuitive_next_prev/script.js
@@ -1,1 +1,30 @@
-// Empty content script, in case you want to mess with the dom or pass messages back to bg.js
+// First determine if page has next and/or prev/previous text as a link to some page within the same domain.
+// If so, bind keys such that pressing ▶ will navigate to the next page and ◀ vice versa.
+
+var next_href = false;
+var prev_href = false;
+
+$('a').each(function(){
+
+	var link_text = $.trim($(this).text());
+
+	var next = link_text.match(/^(next)$/i);
+	if(next !== null){
+		next_href = $(this).attr("href");
+	}
+
+	var prev = link_text.match(/^(prev|previous)$/i);
+	if(prev !== null){
+		prev_href = $(this).attr("href");
+	}
+	
+});
+
+$(document).live('keydown', function (e) {
+	if ( e.keyCode == 39 && next_href !== false){
+		window.location = next_href;
+	}
+	if ( e.keyCode == 37 && prev_href !== false){
+		window.location = prev_href;
+	}
+});
