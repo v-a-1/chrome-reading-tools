ff3c2257b277a3be292e6c5182f8901eae3e8a38
v-a-1 <varun.m.ajani@gmail.com>
2012-10-15 13:47:45 +0530

    Trying with jquery.hotkeys. What a badly implemented solution. Doesnt work with + - etc keys.

--- reading_tools/html_width.js
+++ reading_tools/html_width.js
@@ -5,54 +5,35 @@
 // This might not work correctly if any css properties have been attached to <html>
 // The change is persistent. i.e. It is remembered across any domain whose html width was once modified.
 
-// -- Keep track of which keys have been pressed.
-// Thus enabling detection of multiple key presses.
-var keys = {};
-
-$(document).keydown(function (e) {
-    keys[e.which] = true;
-});
-
-$(document).keyup(function (e) {
-    delete keys[e.which];
-});
-// -- Keys tracking end
-
-var existing_width = $('html').width();
 // Set any pre-existing width for this domain.
 // Using sync so this data is available across browsers.
 chrome.storage.sync.get('html_width', function(items){
 	var html_width = items.html_width !== undefined ? items.html_width : false;
-    existing_width = (html_width !== false && html_width[document.domain] !== undefined) ? html_width[document.domain] : false;
+    var existing_width = (html_width !== false && html_width[document.domain] !== undefined) ? html_width[document.domain] : false;
 	$('html').css({'width': existing_width, 'margin': '0 auto'});
 });
 
-var html_width = existing_width;
-// shift: 16, control: 17, +: 187, -:189, 0:48
-$(document).keydown(function (e) {
-	if(16 in keys && 17 in keys){ // Shift Control
-		if(187 in keys){ // Increase html width '+'
-			$('html').css({'width': html_width + 80, 'margin': '0 auto'});
-			html_width = $('html').width();
-			save_html_width(html_width);
-		}else if(189 in keys){ // Decrease html width '-'
-			$('html').css({'width': html_width - 80, 'margin': '0 auto'});
-			html_width = $('html').width();
-			save_html_width(html_width);
-		}else if(48 in keys){ // Reset html width '0'
-			$('html').css({'width': 'auto', 'margin': 'auto'});
-			html_width = $('html').width();
-			save_html_width(html_width);
-		}
-	}
+$(document).bind('keydown.alt_ctrl_+', function(){
+	console.log('a hit');
+	$('html').css({'width': $('html').width() + 80, 'margin': '0 auto'});
+	save_html_width($('html').width());
+});
+
+$(document).bind('keydown.alt_ctrl_=', function(){
+	$('html').css({'width': $('html').width() - 80, 'margin': '0 auto'});
+	save_html_width($('html').width());
+});
+
+$(document).bind('keydown.ctrl_shift_0', function(){
+	$('html').css({'width': 'auto', 'margin': 'auto'});
+	save_html_width($('html').width());
 });
 
 // Save modified width to 'domain : width' pair in synced chrome storage.
 function save_html_width(width){
-	var html_width = {};
 	chrome.storage.sync.get('html_width', function(items){
-		html_width = items !== undefined ? items : {};
+		var html_width = items !== undefined ? items : {};
+		html_width[document.domain] = width;
+		chrome.storage.sync.set({'html_width': html_width});
 	});
-	html_width[document.domain] = width;
-	chrome.storage.sync.set({'html_width': html_width});
 }
